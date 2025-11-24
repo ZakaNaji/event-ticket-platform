605acef29e963db544c83e9cc848715dba182ec4
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-11-24 14:57:44 +0100

    feat(api): events controller: create event

---
+++ api/src/main/java/com/znaji/event_ticket_platform/api/events/EventsController.java
@@ -0,0 +1,34 @@
+package com.znaji.event_ticket_platform.api.events;
+
+import com.znaji.event_ticket_platform.application.events.AddTicketTypeCommand;
+import com.znaji.event_ticket_platform.application.events.CreateEventCommand;
+import com.znaji.event_ticket_platform.application.events.EventApplicationService;
+import com.znaji.event_ticket_platform.common.mapping.events.EventMapper;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventsController {
+
+    private final EventApplicationService eventApplicationService;
+
+    public EventsController(EventApplicationService eventApplicationService) {
+        this.eventApplicationService = eventApplicationService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<UUID> createEvent(@Valid @RequestBody CreateEventRequest request) {
+        CreateEventCommand createEventCommand = EventMapper.toCommand(request);
+        UUID result = eventApplicationService.createEvent(createEventCommand);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(result);
+    }
+
+    
+}
