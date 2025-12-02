a7f388391a0737f526c401804a9c7a8a4d47af0c
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-02 20:29:42 +0100

    feat(api): create order domain tests

---
+++ api/src/test/java/com/znaji/event_ticket_platform/domain/orders/OrderTest.java
@@ -0,0 +1,45 @@
+package com.znaji.event_ticket_platform.domain.orders;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    // Utility data creator
+    private OrderItem item(UUID ticketTypeId, int quantity, BigDecimal price) {
+        return OrderItem.createItem(ticketTypeId, quantity, price, "Some Ticket");
+    }
+
+    @Test
+    void createOrderWithValidItemsShouldSucceed() {
+
+        UUID attendeeId = UUID.randomUUID();
+        UUID eventId = UUID.randomUUID();
+
+        OrderItem item = item(UUID.randomUUID(), 2, new BigDecimal("100.00"));
+
+        Order order = Order.createOrder(
+                attendeeId,
+                eventId,
+                List.of(item)
+        );
+
+        assertEquals(order.getStatus(), OrderStatus.PENDING);
+        assertEquals(order.getTotalAmount(), new BigDecimal("200.00"));
+        assertEquals(order.getItems().size(), 1);
+    }
+
+    @Test
+    void createOrderFailsIfItemsEmpty() {
+        UUID attendeeId = UUID.randomUUID();
+        UUID eventId = UUID.randomUUID();
+
+        assertThrows(IllegalArgumentException.class, () -> Order.createOrder(attendeeId, eventId, List.of()));
+    }
+
+}
